de9daa39a5bfd1cdee0bd5b1ad0386b01ff2ce05
dexkode4 <dexkode4@gmail.com>
2020-11-06 18:48:00 +0100

    custom button

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { HiArrowCircleRight } from "react-icons/hi";
+
+interface IButton {
+	text: string;
+    type: string
+}
+const ButtonType: Record<string, string> = {
+    isQuiz : " bg-white hover:bg-blue-900 text-blue-900 hover:text-white hover:border-blue-900 rounded-full",
+    isClass: " bg-green-100 hover:bg-green-100 text-green-500 hover:text-green-500 hover:border-green-500 rounded-xl",
+    default: " bg-green-600 hover:bg-white text-white hover:text-green-500 hover:border-green-500 rounded-xl",
+    isCoin: ""
+}
+
+function Button({ text, type = "default"}: IButton) {
+	return (
+		<button
+
+			className={`m-4 flex items-center justify-between font-semibold  py-2 px-6 border ${ButtonType[`${type}`]}`}
+		>
+			{text}
+			{type === "isQuiz" && <HiArrowCircleRight className="ml-2 text-3xl" />}
+		</button>
+	);
+}
+
+export default Button;
