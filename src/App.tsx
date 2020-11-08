7290e82304166dcc407f1ff1845d7411dbf66874
teeliny <teeliny@gmail.com>
2020-11-08 10:09:49 +0100

    Added images and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Button} from './components/atoms/Button';
+// import {Button} from './components/atoms/Button';
+// import Search from './components/atoms/Search';
+// import ClassSideBar from './components/molecules/ClassSideBar';
+// import CardWithText from './components/atoms/CardWithText';
+import ClassPage from './components/organisms/ClassPage';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Button text="Join Live" type="isQuiz"/>
+      {/* <Button text="Join Live" type="isQuiz" /> */}
+      {/* <Search placeholder="Search Menu...." width="quaterSize"/> */}
+      {/* <ClassSideBar /> */}
+      {/* <CardWithText url="/assets/img/Chemistry.svg" text="Chemistry"/> */}
+      <ClassPage />
    </>
   );
 }
